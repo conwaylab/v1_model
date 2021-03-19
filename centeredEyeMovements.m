147df0278cfcb8b40ef116cd685eb629c324c180
Singh <singhsr@nih.gov>
2021-03-19 14:19:06 -0400

    wrote centeredEyeMovements.m

---
+++ centeredEyeMovements.m
@@ -0,0 +1,15 @@
+function spirals = centeredEyeMovements(spiralParams,numMovs,movDist,center)
+
+th = spiralParams.th;
+b = spiralParams.b;
+a = spiralParams.a;
+
+spirals = struct('coords',[],'angles',[],'theta',[],'radius',[],'center',[]);
+
+spirals(1) = logSpiral(th,b,a,center);
+
+for i = 2:numMovs+1
+    moveAngle = rand * 2*pi;
+    gazeCenter = center + [cos(moveAngle),sin(moveAngle)]*movDist;
+    spirals(i) = logSpiral(th,b,a,gazeCenter);
+end
